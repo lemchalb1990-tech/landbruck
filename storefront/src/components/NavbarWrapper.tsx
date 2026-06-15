07e13e88d40ab2d1ade3998af6698f1ea8f63f7a
lemchalb1990-tech <lemchalb.1990@gmail.com>
2026-06-14 22:22:48 -0400

    fix: disable cache in NavbarWrapper so logo changes reflect immediately

--- storefront/src/components/NavbarWrapper.tsx
+++ storefront/src/components/NavbarWrapper.tsx
@@ -1,7 +1,9 @@
+import { unstable_noStore as noStore } from 'next/cache'
 import { prisma } from '@/lib/prisma'
 import Navbar from './Navbar'
 
 export default async function NavbarWrapper() {
+  noStore()
   const configs = await prisma.siteConfig.findMany()
   const configMap = Object.fromEntries(configs.map(c => [c.key, c.value as unknown]))
   const logo = configMap.logo as { type: 'text' | 'image'; value: string } | undefined
